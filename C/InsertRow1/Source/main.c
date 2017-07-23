c8c6c20ec4f301999ac7b6e7407a83f163f38c18
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-07-23 13:57:54 +0100

    Added file

---
+++ C/InsertRow1/Source/main.c
@@ -0,0 +1,54 @@
+/*
+    Copyright (c) 2015-2017 Xavier Leclercq
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+    IN THE SOFTWARE.
+*/
+
+#include <sqlite3.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    sqlite3* db = NULL;
+
+    int rc = sqlite3_open_v2("../../Data/People.db", &db, SQLITE_OPEN_READWRITE, NULL);
+    if (rc != 0)
+    {
+        printf("Error creating database handle (return code: %d, %s)\n", rc, sqlite3_errstr(rc));
+    }
+    else
+    {
+        char* sql = "INSERT INTO PERSONS (FIRSTNAME) VALUES ('Emma');";
+        char* errorMessage = 0;
+        rc = sqlite3_exec(db, sql, 0, 0, &errorMessage);
+        if (rc != 0)
+        {
+            printf("Error inserting row (return code: %d, %s)\n", rc, sqlite3_errstr(rc));
+            if (errorMessage)
+            {
+                printf("Error message: %s\n", errorMessage);
+            }
+        }
+        sqlite3_free(errorMessage);
+    }
+
+    sqlite3_close(db);
+
+    return 0;
+}
